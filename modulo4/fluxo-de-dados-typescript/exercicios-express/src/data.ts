9c4ec66c79e12eb2ce92906a75b656db6e02daed
Ronald <ronald.santiago46@gmail.com>
2022-03-30 17:36:06 -0300

    Fluxo-de-dados

---
+++ modulo4/fluxo-de-dados-typescript/exercicios-express/src/data.ts
@@ -0,0 +1,7 @@
+export type Produto ={
+   id: number,
+   name: string,
+   price: number
+}
+
+export let arrayProdutos: Produto[]= [{id:1, name:'Ronald', price: 23}, {id:2, name:'Flávia', price: 25}, {id:3, name:'Bruna', price: 35}]
