e7dc51de4ffbc7a98a63dba8fd8ef36e1a7822af
Ronald <ronald.santiago46@gmail.com>
2022-04-04 07:40:37 -0300

    Atualização

--- modulo4/sistema-bancario/sistema/src/index.ts
+++ modulo4/sistema-bancario/sistema/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'; 
+import express from 'express';
 import cors from 'cors';
 
 
@@ -9,13 +9,13 @@ app.use(cors());
 
 app.listen(3004, () => {
     console.log("Server funfa");
-   });
+});
 
-   type extrato = {
+type extrato = {
     valor: number,
     descricao: string,
     dataExtrato: string
-   }
+}
 
 type userType = {
     nome: string,
@@ -25,18 +25,18 @@ type userType = {
     extrato: extrato[]
 }
 
-let users: userType[] =[{
+let users: userType[] = [{
     nome: "Ronald",
     cpf: 1672,
     data: "21/06/22",
     saldo: 21344,
-    extrato:[{
+    extrato: [{
         valor: 342,
         descricao: "agua",
-        dataExtrato:"21/11/2000"
+        dataExtrato: "21/11/2000"
     }
 
-]
+    ]
 },
 
 {
@@ -44,87 +44,108 @@ let users: userType[] =[{
     cpf: 1555,
     data: "21/07/08",
     saldo: 21344,
-    extrato:[{
+    extrato: [{
         valor: 333,
         descricao: "terra",
-        dataExtrato:"21/12/1997"
+        dataExtrato: "21/12/1997"
     }
 
-]
+    ]
 }
 
 ]
 
 
 
-app.post("/usersr", (req, res) =>{
-    try{
-  let novoUsuario: userType ={
-      nome: req.body.nome,
-      cpf: req.body.cpf,
-      data: req.body.data,
-      saldo: 0,
-      extrato:[]
-    
+//EXERCÍCIO 1 - CRIAR A CONTA
+app.post("/usersr", (req, res) => {
+    try {
+        let novoUsuario: userType = {
+            nome: req.body.nome,  //Não preciso ao criar um novo objeto com tipo, ter de dar todos os recursos de requisição
+            cpf: req.body.cpf,
+            data: req.body.data,
+            saldo: 0,
+            extrato: []
+
+        }
+
+
+        let anoTempo = new Date().getTime()
+        let [dia, mes, ano] = req.body.data.split("/")
+        let anoNascimento: string = ano + "-" + mes + "-" + dia
+        let idade = Date.parse(anoNascimento)
+
+        if (anoTempo - idade < 18 * 31536000000) {
+
+            throw new Error("Sua idade não é permitida.")
+        }
+
+
+        users.push(novoUsuario)
+        res.status(200).send(users)  //Aqui é onde será de fato o envio da minha reuisição
+
+    } catch (error: any) {
+        res.send(error.message)
+
     }
 
-   
-    let anoTempo = new Date().getTime()
-    let [dia, mes, ano] = req.body.data.split("/")
-    let anoNascimento: string = ano+"-"+mes+"-"+dia
-    let idade = Date.parse(anoNascimento)
-   
-    if (anoTempo - idade < 18*31536000000){
-        
-        throw new Error("Sua idade não é permitida.")
-    }
-
-
-     users.push(novoUsuario)
-
-    res.status(200).send(users)
-}catch(error: any){
-    res.send(error.message)
-
-}
-   
 })
 
-app.get("/users/:idCpf", (req,res) =>{
-    const nome = req.query.nome as string
+//EXERCÍCIO 2- PEGAR SALDO
+app.get("/users/:idCpf", (req, res) => {
+    const nome = req.query.nome as string  //O query serve para realizar pesquisa
     const cpf = Number(req.params.idCpf)
 
-    let novaLista = users.filter((user) =>{
-        
-       if(user.nome === nome && user.cpf === cpf){
-           return user
-       }
-    
-     }).map((user) =>{
-         return user
+    let novaLista = users.filter((user) => {
+        if (user.nome === nome && user.cpf === cpf) {
+            return user
+        }
+    }).map((user) => {
+        return user
 
-
-     })  
-   res.send(novaLista)
-
-
-        
-
+    })
+    res.send(novaLista)
 
 })
 
-//O usuário deve conseguir adicionar saldo à sua conta, passando seu nome, o CPF e o valor que desejar.
-app.get("/users/:idSaldo", (req,res) =>{
+//EXERCÍCIO 3- ADICIONAR SALDO
+app.get("/users/:idSaldo", (req, res) => {
     const nome = req.query.nome as string
     const cpf = Number(req.query.cpf)
-    const saldo = Number(req.params.idSaldo) 
+    const saldo = Number(req.params.idSaldo)
 
-    users.forEach((user) =>{
-       if(user.nome === nome || user.cpf === cpf){
-           user.saldo = saldo
+    users.forEach((user) => {
+        if (user.nome === nome || user.cpf === cpf) {
+            user.saldo = saldo
 
-       }
-    })   
-  res.send(users)
+        }
+    })
+    res.send(users)
+
+})
+
+//Exercício 4 - Pagar uma conta
+app.post("/pagarconta", (req, res) =>{
+     let valor = req.body.valor
+     let descricao = req.body.descricao
+     let dataExtrato = req.body.dataExtrato
+
+     let pagamento: userType = {
+        nome: "",  
+        cpf: 0,
+        data: "",
+        saldo: 0,
+        extrato: [valor = valor,
+            descricao = descricao,
+            dataExtrato = dataExtrato
+        ]
+
+    }
+
+       users.push(pagamento)
+
+       res.send(users)
+
+
 
 })
