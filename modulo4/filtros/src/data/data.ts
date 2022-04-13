7d079a303f69b6e5589d3d61917a2e73cd517d01
Ronald <ronald.santiago46@gmail.com>
2022-04-13 16:07:00 -0300

    filtros-ordenacao

--- modulo4/filtros/src/data/data.ts
+++ modulo4/filtros/src/data/data.ts
@@ -9,13 +9,10 @@ export const connection = knex({
 	connection: {
     host: process.env.DB_HOST,
     port: 3306,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 
-    // host : '35.226.146.116',
-    user : '21713049-ronald-santiago',
-    password : "WGEL88c6UazK@ocZ!Hdw",
-    database : 'vaughan-21713049-ronald-santiago',
+  
   }
 });
